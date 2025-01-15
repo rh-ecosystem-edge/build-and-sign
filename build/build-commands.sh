7423cc103ed8aa7a3e9c9700d049ef0919753d1f
Enrique Belarte Luque <ebelarte@redhat.com>
2025-01-15 12:45:02 +0100

    Add kernel sources directory
    Signed-off-by: Enrique Belarte Luque <ebelarte@redhat.com>

--- build/build-commands.sh
+++ build/build-commands.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+BUILD_KERNEL_VER=$(cat /tmp/BUILD_KERNEL_VER)
 log_error() {
     echo "$(date +'%Y-%m-%d %H:%M:%S') ERROR: $@" >&2
 }
 
-if ! make modules -j$(nproc) SYSSRC=/usr/src/kernels/$KERNEL_VERSION; then
+if ! make modules -j$(nproc) SYSSRC=/usr/src/kernels/$BUILD_KERNEL_VER; then
     log_error "The build command failed."
     exit 1
 fi
