5acfb5ac3f722d9e7a9b4e44d144a189a1c31a17
Enrique Belarte Luque <ebelarte@redhat.com>
2025-01-29 13:54:18 +0100

    Changes to check script
    Signed-off-by: Enrique Belarte Luque <ebelarte@redhat.com>

--- scripts/check-changes.py
+++ scripts/check-changes.py
@@ -1,9 +1,17 @@
 import json
 import subprocess
+import requests
+import os
 
 # File paths
 matrix_json_file = "data/combined_output.json"
 argsfile = "argfile.conf"
+token = os.getenv("TOKEN")
+api = os.getenv("GITHUB_API_URL")
+dtk_reg = "quay.io/build-and-sign/pa-driver-toolkit"
+
+if not token:
+    raise ValueError("GITHUB_TOKEN is missing!")
 
 # Function to update the DRIVER_PUBLISHED field and DRIVER_VERSION file
 def update_files(driver_version, kernel_version):
@@ -27,6 +35,9 @@ def update_files(driver_version, kernel_version):
         for line in lines:
             if line.startswith("DRIVER_VERSION="):
                 f.write(f"DRIVER_VERSION={driver_version}\n")
+            elif line.startswith("DTK_IMAGE="):
+                dtk_image = f"{dtk_reg}:{kernel_version}"
+                f.write(f"DTK_IMAGE={dtk_image}\n")                
             else:
                 f.write(line)
 
@@ -49,6 +60,26 @@ def create_branch_and_pr(driver_version, kernel_version):
     # Push branch
     subprocess.run(["git", "push", "origin", branch_name], check=True)
 
+    # Create the PR for each commit
+    url = api +"/repos/rh-ecosystem-edge/build-and-sign/pulls"
+    headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {token}",
+    "X-GitHub-Api-Version": "2022-11-28"            
+    }
+    data = {
+    "title": f"Automated build-and-sign PR for {branch_name}",
+    "body": f"This PR was created automatically by GitHub Actions to trigger a new build and sign of {branch_name}.",
+    "head": f"{branch_name}",
+    "base": "main"
+     }
+    #print(f"REQUEST: {title} | Headers: {headers} | Data: {data} | URL: {url}")
+    pr = requests.post(url, headers=headers, json=data)
+    if pr.status_code == 201:
+       print(pr.json)
+    else:
+       raise SystemExit(f'Error: Got Status {pr.status_code}.')
+    
 # Main script
 if __name__ == "__main__":
     # Load current combined JSON data
